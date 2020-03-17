72ec564264d943516b95050a8722a68b5e93e9ad
Albert Lin <albert.yp.lin@gmail.com>
2020-03-17 21:31:04 +0000

    fix(config): Get environment variable by getenv

--- sitnow_project/sitnow_project/config/prod.py
+++ sitnow_project/sitnow_project/config/prod.py
@@ -3,5 +3,5 @@ import os
 environment = "production"
 IS_DEBUG = False
 
-GOOGLE_API_KEY = os.environ("GOOGLE_API_KEY")
-GOOGLE_JS_API_KEY = os.environ("GOOGLE_JS_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_JS_API_KEY = os.getenv("GOOGLE_JS_API_KEY")
